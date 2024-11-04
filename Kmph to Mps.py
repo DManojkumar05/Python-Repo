db495a900dc464685f15969db58f407a7826824e
DManojkumar05 <23P31A4411@acet.ac.in>
2024-11-04 22:03:48 +0530

    Uploading code: Kmph to Mps.py

---
+++ Kmph to Mps.py
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        double res=s/3.6;
+        System.out.printf("%.2f",res);
+    }
+}
