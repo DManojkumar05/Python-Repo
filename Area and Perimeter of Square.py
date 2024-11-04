3a885302190bb9cc3c9ce55bbe77632de2c2a035
DManojkumar05 <23P31A4411@acet.ac.in>
2024-11-04 21:59:45 +0530

    Uploading code: Area and Perimeter of Square.py

---
+++ Area and Perimeter of Square.py
@@ -0,0 +1,9 @@
+import java.util.*;
+public class main{
+    public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    System.out.printf("%d",a*a);
+    System.out.printf(" %d",4*a);
+}
+}
