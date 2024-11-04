3793369bf31dc46c8198d9ae9d0e6f59d507e61e
DManojkumar05 <23P31A4411@acet.ac.in>
2024-11-04 22:13:30 +0530

    Uploading code: Capacity.py

---
+++ Capacity.py
@@ -0,0 +1,11 @@
+import java.util.*;
+public class main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        int s=sc.nextInt();
+        int k=sc.nextInt();
+        int c=t*s*k*1;
+        System.out.println(c+" KB");
+    }
+}
