8425b7fed7057dca0637ca39969d308d5d8a0672
DManojkumar05 <23P31A4411@acet.ac.in>
2024-11-04 22:35:59 +0530

    Uploading code: Average Weight.py

---
+++ Average Weight.py
@@ -0,0 +1,11 @@
+import java.util.*;
+public class main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int g=sc.nextInt();
+        int b=sc.nextInt();
+        int w=(3*a)-(g+b);
+        System.out.println(w);
+    }
+}
